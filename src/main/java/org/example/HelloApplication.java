8fca3ce3f72bd7b33271ee3fa9d2d71e8e7c3aed
Justin <zhou.jiad@northeastern.edu>
2023-08-03 14:48:01 -0700

    Hello World Application, for Spring boot Linux test.

---
+++ src/main/java/org/example/HelloApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello() {
+        System.out.println("hello world ... !!! ");
+        return "OK >>> !!! Success";
+    }
+}
